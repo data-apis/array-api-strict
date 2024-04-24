1c4460d8abeeaf8235ef1fd081ff1a56667f844e
Aaron Meurer <asmeurer@gmail.com>
2024-04-24 16:13:53 -0600

    Add searchsorted
    As far as I can tell, except for the dtype restriction, the standard is the
    same as NumPy.

--- array_api_strict/__init__.py
+++ array_api_strict/__init__.py
@@ -286,9 +286,9 @@ from ._manipulation_functions import (
 
 __all__ += ["concat", "expand_dims", "flip", "moveaxis", "permute_dims", "repeat", "reshape", "roll", "squeeze", "stack"]
 
-from ._searching_functions import argmax, argmin, nonzero, where
+from ._searching_functions import argmax, argmin, nonzero, searchsorted, where
 
-__all__ += ["argmax", "argmin", "nonzero", "where"]
+__all__ += ["argmax", "argmin", "nonzero", "searchsorted", "where"]
 
 from ._set_functions import unique_all, unique_counts, unique_inverse, unique_values
 
