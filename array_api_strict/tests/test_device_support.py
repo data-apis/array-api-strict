032f3bb8e4623a9a080d89ffbcd6b11fafddb336
Tim Head <betatim@gmail.com>
2024-10-03 15:15:27 +0200

    Formatting

--- array_api_strict/tests/test_device_support.py
+++ array_api_strict/tests/test_device_support.py
@@ -3,13 +3,27 @@ import pytest
 import array_api_strict
 
 
-@pytest.mark.parametrize("func_name", ("fft", "ifft", "fftn", "ifftn", "irfft",
-                                  "irfftn", "hfft", "fftshift", "ifftshift"))
+@pytest.mark.parametrize(
+    "func_name",
+    (
+        "fft",
+        "ifft",
+        "fftn",
+        "ifftn",
+        "irfft",
+        "irfftn",
+        "hfft",
+        "fftshift",
+        "ifftshift",
+    ),
+)
 def test_fft_device_support_complex(func_name):
     func = getattr(array_api_strict.fft, func_name)
-    x = array_api_strict.asarray([1, 2.],
-                                 dtype=array_api_strict.complex64,
-                                 device=array_api_strict.Device("device1"))
+    x = array_api_strict.asarray(
+        [1, 2.0],
+        dtype=array_api_strict.complex64,
+        device=array_api_strict.Device("device1"),
+    )
     y = func(x)
 
     assert x.device == y.device
@@ -18,8 +32,7 @@ def test_fft_device_support_complex(func_name):
 @pytest.mark.parametrize("func_name", ("rfft", "rfftn", "ihfft"))
 def test_fft_device_support_real(func_name):
     func = getattr(array_api_strict.fft, func_name)
-    x = array_api_strict.asarray([1, 2.],
-                                 device=array_api_strict.Device("device1"))
+    x = array_api_strict.asarray([1, 2.0], device=array_api_strict.Device("device1"))
     y = func(x)
 
     assert x.device == y.device
