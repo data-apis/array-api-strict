b2e3ecc4eaa62f496f7eeda2ca1a72c20bbb065c
Aaron Meurer <asmeurer@gmail.com>
2024-11-04 12:52:07 -0700

    Require numeric types in diff

--- array_api_strict/_utility_functions.py
+++ array_api_strict/_utility_functions.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from ._array_object import Array
 from ._flags import requires_api_version
+from ._dtypes import _numeric_dtypes
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -49,6 +50,12 @@ def diff(
     prepend: Optional[Array] = None,
     append: Optional[Array] = None,
 ) -> Array:
+    if x.dtype not in _numeric_dtypes:
+        raise TypeError("Only numeric dtypes are allowed in diff")
+
+    # TODO: The type promotion behavior for prepend and append is not
+    # currently specified.
+
     # NumPy does not support prepend=None or append=None
     kwargs = dict(axis=axis, n=n)
     if prepend is not None:
