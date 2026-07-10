43edaaa71084ea470703afa1dad082cb074c1f27
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2026-07-10 13:28:21 +0200

    Revert "manually sync dlpack enum updates to _devices.py"
    This reverts commit 083212060b10d00fda24553937ae97d9e96e4062.

--- array_api_strict/_devices.py
+++ array_api_strict/_devices.py
@@ -1,5 +1,4 @@
 from typing import Final
-from enum import IntEnum
 
 from ._dtypes import (
     DType, float32, float64, complex64, complex128, int64,
@@ -35,34 +34,6 @@ CPU_DEVICE = Device()
 NO_FLOAT64_DEVICE = Device("no_float64")
 
 ALL_DEVICES = (CPU_DEVICE, Device("device1"), Device("device2"), NO_FLOAT64_DEVICE)
-
-class DLDeviceType(IntEnum):
-    kDLCPU = 1
-    kDLCUDA = 2
-
-
-_DLPACK_DEVICE_FOR: Final[dict[Device, tuple[DLDeviceType, int]]] = {
-    CPU_DEVICE: (DLDeviceType.kDLCPU, 0),
-    Device("device1"): (DLDeviceType.kDLCUDA, 0),
-    Device("device2"): (DLDeviceType.kDLCUDA, 1),
-}
-
-_DLPACK_DEVICE_TO_LOGICAL: Final[dict[tuple[int, int], Device]] = {
-    (int(device_type), device_id): logical_device
-    for logical_device, (device_type, device_id) in _DLPACK_DEVICE_FOR.items()
-}
-
-
-def _normalize_dl_device(device_type: IntEnum | int, device_id: int) -> tuple[int, int]:
-    return (int(device_type), device_id)
-
-
-def _device_from_dlpack_device(
-    device_type: IntEnum | int, device_id: int
-) -> Device:
-    return _DLPACK_DEVICE_TO_LOGICAL.get(
-        _normalize_dl_device(device_type, device_id), CPU_DEVICE
-    )
 
 
 def check_device(device: Device | None) -> None:
